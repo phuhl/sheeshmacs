5b6638526a5c3c4622ba839ac0402b6d8a7e2331
Philipp Uhl <philipp.uhl@rwth-aachen.de>
2016-05-27 13:36:28 +0200

    Starter-kit basics added (and reduced)

---
+++ custom.el
@@ -0,0 +1,11 @@
+(custom-set-variables
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
+ '(ansi-color-names-vector
+   ["#212526" "#ff4b4b" "#b4fa70" "#fce94f" "#729fcf" "#e090d7" "#8cc4ff" "#eeeeec"])
+ '(custom-enabled-themes (quote (tsdh-dark)))
+ '(menu-bar-mode nil)
+ '(scroll-bar-mode nil)
+ '(tool-bar-mode nil))
+(custom-set-faces
+ )
