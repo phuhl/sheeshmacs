bd323173eaed88d46ae3ca0bdb7b10a510b00435
Philipp Uhl <git@ph-uhl.com>
2017-11-14 10:38:43 +0100

    Tern, rjsx-mode, minor changes

--- init.el
+++ init.el
@@ -6,6 +6,13 @@
 ;;
 
 ;; load Org-mode from source when the ORG_HOME environment variable is set
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
 (when (getenv "ORG_HOME")
   (let ((org-lisp-dir (expand-file-name "lisp" (getenv "ORG_HOME"))))
     (when (file-directory-p org-lisp-dir)
